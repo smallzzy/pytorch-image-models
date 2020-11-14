65afdd6b7b89c1be29fec1ab3575d69ef3b9a858
Zhimin Tang <1003306213@qq.com>
2020-11-14 16:51:07 +0800

    update

--- timm/utils/quant.py
+++ timm/utils/quant.py
@@ -19,7 +19,7 @@ def get_qconfig(weight_bw, pot):
         bw=weight_bw,
         is_weight=True,
         alpha_pot=pot,
-        decay=0.98
+        decay=0.9
     )
 
     rcf_weight_8bit = kqat.RCF.with_args(
@@ -29,7 +29,7 @@ def get_qconfig(weight_bw, pot):
         bw=8,
         is_weight=True,
         alpha_pot=pot,
-        decay=0.98
+        decay=0.9
     )
 
     qcfg = kqat.KQConfig(
@@ -50,8 +50,14 @@ def attach_qconfig(args, model):
     if "mobilenetv2" in args.model:
         model.qconfig = qcfg
         model.conv_stem.qconfig = qcfg_8bit
-        model.classifier.qconfig = qcfg_8bit
+        # model.classifier.qconfig = qcfg_8bit
     elif "resnet50" in args.model:
         model.qconfig = qcfg
     else:
         raise NotImplementedError("wrong")
+
+    from torch.nn.intrinsic import ConvBn2d, ConvBnReLU2d, ConvReLU2d
+    for mod in model.modules():
+        if isinstance(mod, (ConvBn2d, ConvBnReLU2d, ConvReLU2d)):
+            if mod[0].groups == mod[0].out_channels:
+                mod.qconfig = qcfg_8bit
